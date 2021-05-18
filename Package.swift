5ae01cee258db23c2535f1a04e3edccad860eb49
Alexander Kremenets <kremenets0207@gmail.com>
2021-05-18 12:32:28 +0300

    feat: xcode 12.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 /*
  * Copyright 2017, gRPC Authors All rights reserved.
  *
@@ -8,7 +8,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable la w or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
